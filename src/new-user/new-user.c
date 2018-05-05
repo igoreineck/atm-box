f441c748a8f1bcd9c462290eb477e68214f51f57
Igor Eineck <hauzip@gmail.com>
2018-05-05 18:26:24 -0300

    Criação da funcao para armazenamento de usuario

--- src/new-user/new-user.c
+++ src/new-user/new-user.c
@@ -10,22 +10,44 @@ Opcao 3 (Implícita) - Geração automática de senha (6 dígitos)
 * Após criação, retornar os dados do usuário (somente: nome e cpf)
 
 */
+void save_user(void)
+{
+    // FILE *file;
+
+    // file = fopen("contas/igor/deposito.txt");
+
+    // if (file != NULL)
+}
+
+char password_generate(char *) {}
 
 void new_user(void)
 {
-	char name[20];
-	long int CPF;
-	char password[255];
+	struct user
+	{
+		char name[20];
+		long int CPF;
+		char password[255];
+		char *encrypted_password;
+	};
 
 	printf("Digite seu nome: ");
-	fgets(name, 20, stdin);
+	fgets(user.name, 20, stdin);
 
 	__fpurge(stdin);
 
 	printf("Digite seu CPF: ");
-	scanf("%ld \n", CPF);
+	scanf("%ld \n", user.CPF);
 
-	password = password_generate();
+	printf("Digite a sua senha com no máximo 6 caracteres: ");
+	fgets(user.password, 255, stdin);
+
+	__fpurge(stdin);
+
+	user.encrypted_password = &user.password;
+	password_generate(encrypted_password);
+
+	save_user(user);
 
 	printf("Sua conta foi criada com sucesso!");
 }
--- src/new-user/new-user.h
+++ src/new-user/new-user.h
@@ -1,2 +1,3 @@
 void new_user(void);
-char password_generate(char);
+char password_generate(char *);
+void save_user(void);
