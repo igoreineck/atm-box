47793d2506fa01cc66340485a43cfba9fcc5b70b
Vinicios <vinicios.a.ferreira13@gmail.com>
2018-05-06 15:00:40 -0300

    Criação inicial da pagina para cadastramento de novos usuarios

--- src/new-user/new-user.c
+++ src/new-user/new-user.c
@@ -1,3 +1,8 @@
+//gcc new-user.c -o teste -w
+// comando para compilar, o -w ignora os warnings gerados pelo __fpurge(stdin)
+
+#include <stdio.h>
+#include "new-user.h"
 /* 
 Métodos:
 
@@ -10,44 +15,77 @@ Opcao 3 (Implícita) - Geração automática de senha (6 dígitos)
 * Após criação, retornar os dados do usuário (somente: nome e cpf)
 
 */
-void save_user(void)
+// void save_user(void)
+// {
+   // FILE *file;
+
+     // file = fopen("contas/igor/deposito.txt");
+
+     // if (file != NULL)
+// }
+
+// char password_generate(char *) {}
+
+
+
+
+void main(void)
 {
-    // FILE *file;
+	int cpf_status = 0;
 
-    // file = fopen("contas/igor/deposito.txt");
-
-    // if (file != NULL)
-}
-
-char password_generate(char *) {}
-
-void new_user(void)
-{
-	struct user
+	struct users
 	{
-		char name[20];
-		long int CPF;
+		char name[40];
+		char cpf[12];
 		char password[255];
 		char *encrypted_password;
 	};
 
+	struct users user;
+	
+	system("clear");
+	show_header();
+
 	printf("Digite seu nome: ");
-	fgets(user.name, 20, stdin);
+	fgets(user.name, 40, stdin);
 
 	__fpurge(stdin);
 
-	printf("Digite seu CPF: ");
-	scanf("%ld \n", user.CPF);
+	while(!cpf_status) {
+		printf("Digite seu CPF: ");
+		fgets(user.cpf, 12, stdin);
 
-	printf("Digite a sua senha com no máximo 6 caracteres: ");
-	fgets(user.password, 255, stdin);
+		__fpurge(stdin);
 
-	__fpurge(stdin);
+		if(cpf_verification(user.cpf)) {
+			cpf_status = 1;	
+			
+			system("clear");
+			
+			show_header();
+			
+			printf("Digite seu nome: %s", user.name);
+			printf("Digite seu CPF: %s\n", user.cpf);
+			printf("Digite sua senha: ");
+			
+			fgets(user.password, 255, stdin);
+			
+			__fpurge(stdin);
+			
+		} else {
+			printf("CPF Invalido, Digite novamente\n");
+		}
+	}
 
-	user.encrypted_password = &user.password;
-	password_generate(encrypted_password);
+	// printf("Digite a sua senha com no máximo 6 caracteres: ");
+	// fgets(user.password, 255, stdin);
 
-	save_user(user);
+	// __fpurge(stdin);
 
-	printf("Sua conta foi criada com sucesso!");
+	// user.encrypted_password = &user.password;
+	// // password_generate(encrypted_password);
+
+	// // save_user(user);
+
+	// printf("Sua conta foi criada com sucesso!");
 }
